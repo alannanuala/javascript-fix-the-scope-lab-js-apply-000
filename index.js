edaa46bd09112e8d4d925387e14475a8d043e101
victorious-firmware-2064 <alanna.nuala@gmail.com>
2019-10-09 16:02:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function add2(n) {
 }
 
 
-let theFunk = funkyFunction()
+
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
@@ -25,3 +25,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
+let theFunk = funkyFunction()
